54a971b48ca23e7f9cd998ffba54a69e8ad45cc4
kannna5296 <kannnahacks5296@gmail.com>
2022-06-19 23:39:23 +0900

    sql修正

--- ddl.sql
+++ ddl.sql
@@ -1,15 +1,16 @@
 CREATE TABLE  user (
-   ID INT auto_increment primary key,
+   ID INT IDENTITY(1,1) NOT NULL,
    NAME VARCHAR(255) NOT NULL
 )
 
-CREATE TABLE  task (
-   ID INT auto_increment primary key,
-   created_at DATETIME NOT NULL
+CREATE TABLE task (
+   id INT IDENTITY(1,1) NOT NULL,
+   name varchar(255) NOT NULL,
+   file_id varchar(MAX) NOT NULL
 )
 
 CREATE TABLE task_detail (
-   ID INT auto_increment primary key,
+   id INT IDENTITY(1,1) NOT NULL,
    task_id INT NOT NULL,
    user_id INT NOT NULL,
    content VARCHAR(255) NOT NULL,
